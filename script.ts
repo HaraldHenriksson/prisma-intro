b532fdc633e4695179ff8cb2c1dcf67e1b1216d3
Harald <Harald@espinagar.se>
2023-01-18 16:06:07 +0100

    create user

--- script.ts
+++ script.ts
@@ -2,7 +2,22 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    const user = await prisma.user.findMany()
+    const user = await prisma.user.create({
+        data: {
+            name: "Harald",
+            age: 25,
+            email: "Harald@test.com",
+            UserPreference: {
+                create: {
+                    emailUpdates: true,
+                },
+            },
+        },
+        include: {
+            UserPreference: true,
+        }
+    })
+
     console.log(user)
 }
 
